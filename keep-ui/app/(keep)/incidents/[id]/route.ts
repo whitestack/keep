7c7c1ca90bb133749dd4b1c9a657c4a3b1e2d04c
FelipeAFV <ffigueroa@whitestack.com>
2025-11-25 12:05:07 -0300

    Add ip

--- keep-ui/app/(keep)/incidents/[id]/route.ts
+++ keep-ui/app/(keep)/incidents/[id]/route.ts
@@ -1,22 +1,11 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// type PageProps = {
-//   params: Promise<{ id: string }>;
-//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-// };
+type PageProps = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
-// // This is just a redirect from legacy route
-// export async function GET(request: Request, props: PageProps) {
-//   redirect(`/incidents/${(await props.params).id}/alerts`);
-// }
-
-import { NextRequest, NextResponse } from "next/server";
-import { redirect } from "next/server"; // <-- ESTA ES LA CORRECTA
-
-export function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
-  return redirect(`/incidents/${id}/alerts`);
+// This is just a redirect from legacy route
+export async function GET(request: Request, props: PageProps) {
+  redirect(`/incidents/${(await props.params).id}/alerts`);
 }
