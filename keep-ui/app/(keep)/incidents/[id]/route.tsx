417e915dba5e65ae6d3e486e8e5ab9c7c036a03c
FelipeAFV <ffigueroa@whitestack.com>
2025-11-25 12:42:52 -0300

    Add ip

--- keep-ui/app/(keep)/incidents/[id]/route.tsx
+++ keep-ui/app/(keep)/incidents/[id]/route.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 
 type PageProps = {
   params: Promise<{ id: string }>;
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
 // This is just a redirect from legacy route
